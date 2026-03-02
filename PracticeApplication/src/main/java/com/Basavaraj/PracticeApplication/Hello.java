ff3b778dc714b066adfa649ed2d1f0e05a67122b
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-02 17:08:18 +0530

    create a greeting method using @Getmapping

--- PracticeApplication/src/main/java/com/Basavaraj/PracticeApplication/Hello.java
+++ PracticeApplication/src/main/java/com/Basavaraj/PracticeApplication/Hello.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Hello {
 
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello Basavaraj Welcome to my website";
+    }
+
 
 
 }
