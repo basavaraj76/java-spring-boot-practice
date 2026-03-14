a66fe7ce45a5c995dbbc6a6dd7cf173af9ee7eef
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-14 23:45:24 +0530

    Add new column into the existing table or class

--- PracticeApplication/src/main/java/com/Basavaraj/PracticeApplication/Model/Product.java
+++ PracticeApplication/src/main/java/com/Basavaraj/PracticeApplication/Model/Product.java
@@ -1,13 +1,17 @@
 package com.Basavaraj.PracticeApplication.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
+
 @Entity
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -15,37 +19,13 @@ public class Product {
     private int product_id;
     private String product_name;
     private double price;
+    private String description;
+    private String category;
+    private String brand;
+    private String discount_price;
+    private int stock;
+    private boolean  available;
+    private String image;
+    private String release_date;
 
-    public Product() {
-
-    }
-
-    public Product(int product_id, String product_name, double price){
-        this.product_id = product_id;
-        this.product_name = product_name;
-        this.price = price;
-    }
-
-    public int getProduct_id(){
-        return product_id;
-    }
-    public void setProduct_id(int product_id){
-        this.product_id = product_id;
-    }
-
-    public String getProduct_name() {
-        return product_name;
-    }
-
-    public void setProduct_name(String product_name) {
-        this.product_name = product_name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
